b0be572e7becd2414481b1ab5e9e4a79ce6a00fe
Michael Vincent Ymbong <michael@charlotte.local>
2016-01-14 16:14:06 +0800

    untracked files

---
+++ thisisbullshit/app/src/test/java/com/idearobin/michael/thisisbullshit/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.idearobin.michael.thisisbullshit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
